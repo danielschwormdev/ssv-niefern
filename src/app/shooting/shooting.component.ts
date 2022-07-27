d8f7532e2322170c748019f2b3b0f43563fc7cfb
Daniel_Schworm <daniel.schworm@xim.ag>
2022-07-27 18:03:57 +0200

    first commit

---
+++ src/app/shooting/shooting.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shooting',
+  templateUrl: './shooting.component.html',
+  styleUrls: ['./shooting.component.css']
+})
+export class ShootingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
