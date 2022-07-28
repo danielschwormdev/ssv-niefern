0ade71cc1cda919a5f1416ec94dcbbc6f044d3fa
Daniel_Schworm <daniel.schworm@xim.ag>
2022-07-28 13:05:25 +0200

    Added current version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {StartPageComponent} from "./start-page/start-page.component";
+import {AboutComponent} from "./about/about.component";
+import {CurrentComponent} from "./current/current.component";
+import {ShootingComponent} from "./shooting/shooting.component";
+import {SocietyComponent} from "./society/society.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+	{path: 'start-page', component: StartPageComponent},
+	{path: 'about', component: AboutComponent},
+	{path: 'current', component: CurrentComponent},
+	{path: 'shooting', component: ShootingComponent},
+	{path: 'society', component: SocietyComponent},
+	{path: '', redirectTo: '/start-page', pathMatch: 'full'},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes, {anchorScrolling: 'enabled'})],
+	exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatIconModule} from '@angular/material/icon';
+
+import {HeaderComponent} from './header/header.component';
+import {StartPageComponent} from './start-page/start-page.component';
+import {AboutComponent} from './about/about.component';
+import {FooterComponent} from './footer/footer.component';
+import {CurrentComponent} from './current/current.component';
+import {ShootingComponent} from './shooting/shooting.component';
+import {SocietyComponent} from './society/society.component';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		HeaderComponent,
+		StartPageComponent,
+		AboutComponent,
+		FooterComponent,
+		CurrentComponent,
+		ShootingComponent,
+		SocietyComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		BrowserAnimationsModule,
+		MatIconModule,
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
